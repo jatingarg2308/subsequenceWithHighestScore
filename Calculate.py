239f2fdbacd42268f34f09b4a69dd015cb182fc5
jatingarg2308 <jatingarg2308@gmail.com>
2020-07-12 09:51:17 +0530

    frequency calculation changes

--- Calculate.py
+++ Calculate.py
@@ -18,6 +18,7 @@ def maxValueCalculator(string, number_of_initial_characters):
         for j in range(index + 1, number_of_initial_characters + 1):
             pattern = sub_string[index: j]
             frequency = frequencyStringMatch(pattern, string)
+            print("Strinh {} with frequency {}".format(pattern, frequency))
             if frequency>1:
                 value = calculateValue(len(pattern), frequency)
                 if value > max_value:
@@ -30,50 +31,13 @@ def maxValueCalculator(string, number_of_initial_characters):
 
 
 def frequencyStringMatch(pattern, string):
-    lps = computeLPSArray(pattern)
-    i = 0  # index of string
-    j = 0  # index of pattern
-    result = 0  # number of times string matched
-    next_i = 0
-
-    while i < len(string):
-        if string[i] == pattern[j]:
-            i += 1
-            j += 1
-        if j == len(pattern):
-            j = lps[j - 1]
-            result += 1
-
-            if lps[j] != 0:
-                next_i += 1
-                i = next_i
-            j = 0
-        elif i < len(string) and pattern[j] != string[i]:
-            if j != 0:
-                j = lps[j - 1]
-            else:
-                i += 1
-    return result
-
-
-def computeLPSArray(pattern):
-    n = len(pattern)
-    lps = [0 for i in range(n)]
-    i = 0
-    j = 1
-
-    while j < n:
-        if pattern[i] == pattern[j]:
-            i += 1
-            lps[j] = i
-            j += 1
-        else:
-            if i != 0:
-                i = lps[i - 1]
-            else:
-                lps[j] = i
-                j += 1
-    return lps
+    n=len(string)
+    m=len(pattern)
+    frequency=0
+    for i in range(n-m):
+        if string[i:i+m]==pattern:
+            frequency+=1
+    return frequency
 
 
 def calculateValue(length, frequency):
--- Main.py
+++ Main.py
@@ -1,7 +1,9 @@
+import random
+
 import Calculate
 
 filename="testing.txt"
-number_of_initial_characters=150
+number_of_initial_characters=500
 
 
 Calculate.maxSubstringwithValue(filename, number_of_initial_characters)
